9746dcc5457e8a1aa9cee152bb36d25308b6830d
Alan Acosta <aacosta4@huskers.unl.edu>
2020-08-18 22:31:37 +0000

    initial commit

--- hello.c
+++ hello.c
@@ -1,16 +1,16 @@
 /**
-2 * Author: Alan Acosta 
+ * Author: Alan Acosta 
         Byron Rustrianc
-3 * Date: 20xx/xx/xx
-4 *
-5 * A simple hello world program in C
-6 *
-7 */
-8 #include <stdlib.h>
-9 #include <stdio.h>
-10
-11 int main(int argc, char **argv) {
+ * Date: 20xx/xx/xx
+ *
+ * A simple hello world program in C
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
     printf("Hello World!\n");
-14
-15 return 0;
+
+ return 0;
 }
